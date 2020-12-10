ef6dd22385b990170c65bc871ab0ceaad65c4518
NIKHIL5757H <65679470+NIKHIL5757H@users.noreply.github.com>
2020-12-10 11:09:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,6 +8,6 @@ async def start(client, message):
         [InlineKeyboardButton("Updates Channel", url="https://t.me/Super_botz")],[InlineKeyboardButton("MY CREATOR", url="https://t.me/NGYNY")],
         [InlineKeyboardButton("Support Group", url="https://t.me/super_botz_support")],[InlineKeyboardButton("REPØRT BUGS", url="https://t.me/NGYNY")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis Is YouTube Video Downloader Bot \n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
