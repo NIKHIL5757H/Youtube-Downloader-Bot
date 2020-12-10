3bff360dcea3692ad41368898522f96a31ead496
NIKHIL5757H <65679470+NIKHIL5757H@users.noreply.github.com>
2020-12-10 20:29:45 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,9 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Updates Channel", url="https://t.me/Super_botz")], [InlineKeyboardButton("MY CREATOR", url="https://t.me/NGYNY")]
-        ])
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url And Get File/Video."
     await message.reply_text(helptxt)
